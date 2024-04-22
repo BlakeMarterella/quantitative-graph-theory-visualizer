3576e69a5f4efd1c339b73cc9dee2c657869a89e
Blake M <blake@marterella.com>
2024-04-22 18:11:29 -0400

    Backend: Add support for .env file

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+
+# Load environment variable from the .env file
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return API_KEY
 
 if __name__ == '__main__':
     app.run(debug=True)
